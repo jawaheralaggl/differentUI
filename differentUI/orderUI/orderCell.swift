4c4ba565a614857d0be50ccddcd15217408e63ed
jawaheralaggl <jawaheralaggl@gmail.com>
2020-09-03 17:36:17 +0300

    correct images

--- differentUI/orderUI/orderCell.swift
+++ differentUI/orderUI/orderCell.swift
@@ -51,7 +51,7 @@ class orderCell: UICollectionViewCell {
 extension UIImageView {
     
     func setRounded() {
-        let radius = self.frame.height / 2
+        let radius = self.frame.width / 2
         self.layer.cornerRadius = radius
         self.layer.masksToBounds = true
     }
